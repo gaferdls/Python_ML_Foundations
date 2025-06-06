27b25b6c5edc72b64f5f8fc12625f91e740dd10d
geanpierre <gaferna3@asu.edu>
2025-06-06 01:54:36 -0700

    Set up initial repository structure with topic-based folders

---
+++ opp_concepts/opp_basics.py
@@ -0,0 +1,13 @@
+# opp_basics.py
+
+#Define a simple class:
+class Car:
+    pass
+
+#Create 2 objects (instances) from you Car class
+my_car = Car()
+rental_car = Car()
+
+#You can print them to see they are distinct objects ( their memory address )
+print(my_car)
+print(rental_car)

4efa4d2eed3a423c7bb1bb880355c0144917d299
geanpierre <gaferna3@asu.edu>
2025-06-07 00:38:50 -0700

    Added atributes and methos to Car class, including type of drivetrain

--- opp_concepts/opp_basics.py
+++ opp_concepts/opp_basics.py
@@ -2,12 +2,36 @@
 
 #Define a simple class:
 class Car:
-    pass
+    #The __init__ method is called when a new Car object is created\
+    def __init__(self, make, model, year, color, mileage,wheel_drive):
+            self.make = make
+            self.model = model
+            self.year = year
+            self.color = color
+            self.mileage = mileage
+            self.wheel_drive = wheel_drive
 
-#Create 2 objects (instances) from you Car class
-my_car = Car()
-rental_car = Car()
+    def start_engine(self):
+        return "Vroom! Engine started"
+
+    def accelerate(self, speed_increase):
+        self.mileage += speed_increase
+        return f"Accelerating... Current mileage: {self.mileage}"
+
+    def brake(self):
+        return "Applying brakes..."
+
+    def honk(self):
+        return "Honk! Honk!"
+
+    def get_info(self):
+        return f"{self.make} {self.model} {self.year} ({self.color}), Mileage :  {self.mileage}, {self.wheel_drive}wd"
+
+#Create Car objects, providing initial values for the attributes
+my_car = Car("Toyota", "Camry", 2022, "Silver", 25000.0,2)
+rental_car = Car("Ford", "F-150",2024, "Blue", 10000.0, 4)
 
 #You can print them to see they are distinct objects ( their memory address )
-print(my_car)
-print(rental_car)
+print(my_car.get_info())
+print(rental_car.start_engine())
+print(my_car.accelerate(500.0))
